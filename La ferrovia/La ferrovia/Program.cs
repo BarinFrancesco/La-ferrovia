298ec64f5e0c3a261e15e9501c8e5e467af6504a
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2026-01-27 09:54:26 +0100

    ciao

---
+++ La ferrovia/La ferrovia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace La_ferrovia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
