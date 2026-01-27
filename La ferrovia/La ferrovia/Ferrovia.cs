059dc3a6f9bfaaf1a410e7c538a098aa6632ec15
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2026-01-27 11:39:46 +0100

    bene

---
+++ La ferrovia/La ferrovia/Ferrovia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_ferrovia
+{
+    public class Ferrovia
+    {
+
+        public SemaphoreSlim CodaAttesa;
+        public bool? Direzione;
+
+        public long TreniInMovimento;
+        public long TreniInAttesa;
+
+        public Ferrovia()
+        {
+            CodaAttesa = new SemaphoreSlim(0);
+            TreniInMovimento = 0;
+            TreniInAttesa = 0;
+            Direzione = null;
+        }
+
+        public Task<bool> PuòEntrare(Treno treno)
+        {
+            if(Direzione == treno.Direzione || Direzione == null)
+            {
+                return Task.FromResult(true);
+            } else
+            {
+                return Task.FromResult(false);
+            }
+            
+            
+        }
+    }
+}
---
+++ La ferrovia/La ferrovia/Treno.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_ferrovia
+{
+    public class Treno
+    {
+        public int ID;
+        public bool Direzione { get; private set; }
+        readonly SemaphoreSlim CodaAttesa;
+        public Ferrovia FerroviaDiRiferimento;
+        
+        public Treno(int id ,SemaphoreSlim semaforo, Ferrovia ferrovia)
+        {
+            ID = id;
+            Direzione = Random.Shared.Next(1, 3) % 2 == 0 ? true : false; //direzione casuale 
+            CodaAttesa = semaforo;
+            FerroviaDiRiferimento = ferrovia;
+        }
+
+        public async Task EntraBinario()
+        {
+            while (true)
+            {
+                bool entra = await FerroviaDiRiferimento.PuòEntrare(this);
+
+                if (entra)
+                {
+                    Interlocked.Increment(ref FerroviaDiRiferimento.TreniInMovimento);
+                }
+                else
+                {
+                    await Aspetta();
+                }
+                await Task.Delay(50);
+            }
+             
+        }
+
+
+        public async Task AttraversaBinari()
+        {
+            int tempo = Random.Shared.Next(1, 6);
+            tempo *= 1000;
+
+            await Task.Delay(tempo);
+
+            Interlocked.Decrement(ref FerroviaDiRiferimento.TreniInMovimento);
+
+            if (Interlocked.Read(ref FerroviaDiRiferimento.TreniInMovimento) == 0)
+            {
+                FerroviaDiRiferimento.Direzione = null;
+                CodaAttesa.Release();
+            }
+        }
+
+        public async Task Aspetta()
+        {
+            await CodaAttesa.WaitAsync();
+
+        }
+
+
+    }
+}
